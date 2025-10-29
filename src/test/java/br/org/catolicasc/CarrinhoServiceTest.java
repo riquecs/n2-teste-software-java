78778844e33487ffff9fb6a1f93d15be7d9adcc5
riquecs <henriquesilveira700@gmail.com>
2025-10-29 20:34:39 -0300

    Feature: Adiciona testes parametrizados e corrige dependencias JUnit

--- src/test/java/br/org/catolicasc/CarrinhoServiceTest.java
+++ src/test/java/br/org/catolicasc/CarrinhoServiceTest.java
@@ -1,38 +1,41 @@
 package br.org.catolicasc;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CarrinhoServiceTest {
 
     @Test
-    void deveAplicarPromocaoProgressivaParaTresItens() {
-
-        CarrinhoService service = new CarrinhoService();
-        Produto p1 = new Produto("Produto A", 100.0);
-        Produto p2 = new Produto("Produto B", 100.0);
-        Produto p3 = new Produto("Produto C", 100.0);
-
-        Carrinho carrinho = new Carrinho();
-        carrinho.adicionarItem(p1);
-        carrinho.adicionarItem(p2);
-        carrinho.adicionarItem(p3);
-
-        double total = service.calcularTotal(carrinho);
-
-        assertEquals(270.0, total);
-    }
-
-    @Test
     void deveLancarExcecaoQuandoCarrinhoForNulo() {
-
         CarrinhoService service = new CarrinhoService();
         //Verificamos se a exceção 'IllegalArgumentException' é lançada
         //quando tentamos chamar 'calcularTotal' com um carrinho nulo.
         assertThrows(IllegalArgumentException.class, () -> {
             service.calcularTotal(null);
         });
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2, 100.0, 200.0", //Caso 1: 2 itens (sem desconto)
+            "3, 100.0, 270.0", //Caso 2: 3 itens (10% desconto)
+            "5, 100.0, 450.0", //Caso 3: 5 itens (10% desconto)
+            "0, 100.0, 0.0"    //Caso 4: 0 itens (total 0)
+    })
+    void deveCalcularTotalCorretamenteParaDiferentesQuantidades(int quantidadeItens, double precoItem, double totalEsperado) {
+        CarrinhoService service = new CarrinhoService();
+        Carrinho carrinho = new Carrinho();
+
+        for (int i = 0; i < quantidadeItens; i++) {
+            carrinho.adicionarItem(new Produto("Item", precoItem));
+        }
+
+        double totalCalculado = service.calcularTotal(carrinho);
+
+        assertEquals(totalEsperado, totalCalculado, 0.001);
     }
 }
